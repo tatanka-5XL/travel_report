fd3c60e0f1f65f0a157e94744f1f5061395d1036
Petr Pribil <petr.pribil@profisolv.com>
2025-12-30 11:14:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Welcome to Travelrep
+
+## About
+
+TravelRep is a simple program to generate travel reports. I always hated this stuff so I wanted to create something that would shorten my time spent in the office
+
+### Requirements
+
+Python 3.10+
+
+### Installation
+
+Simply run `main.py`
+
+## Basic principle
+
+The program reads a json file (like `trep.json` in the `Examples`) and generates a txt file with all the basic calculations and info needed
+
+## Future plans
+
+Create a graphic interface for easy data input
+Gereate excel sheets ready to be used
+
+## Author
+
+Tatanka
+p etr.pribil@profisolv.com
+WhatsApp 00420776554003
---
+++ src/main.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import json
+
+
+# Get tomes in a string form
+with open("../input/trep.json", "r", encoding="utf-8") as f:
+    json_file_data = json.load(f)
+
+year = json_file_data["year"]
+
+start_date = json_file_data['trip_info']['start']['date']
+start_time = json_file_data['trip_info']['start']['time']
+
+end_date = json_file_data['trip_info']['end']['date']
+end_time = json_file_data['trip_info']['end']['time']
+
+
+# Start and End isodates
+start_dt = datetime.strptime(
+    f"{year}{start_date}{start_time}", "%Y%m%d%H%M")
+start_iso = start_dt.strftime("%Y-%m-%dT%H:%M")
+
+end_dt = datetime.strptime(
+    f"{year}{end_date}{end_time}", "%Y%m%d%H%M")
+end_iso = end_dt.strftime("%Y-%m-%dT%H:%M")
+
+
+print(start_iso)
+print(end_iso)
+
+
+# First segment
+
+
+# Get the difference between times
+start = datetime.fromisoformat("2025-03-12T06:30")
+end = datetime.fromisoformat("2025-03-13T09:10")
+
+delta = end - start
+hours = delta.total_seconds() / 3600
+
+print(hours)   # 2.6666666666666665
